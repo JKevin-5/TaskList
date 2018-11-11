aa85a9b0e1127e38774ff072fb64f01756a847d1
kevin Jiang <15810880126@163.com>
2018-11-11 11:30:20 +0800

    update

--- Inventory/app/src/main/java/com/example/user/inventory/Fragment1.java
+++ Inventory/app/src/main/java/com/example/user/inventory/Fragment1.java
@@ -1,150 +1,209 @@
 package com.example.user.inventory;
 
+import android.Manifest;
+import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.res.AssetFileDescriptor;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
+
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-import model.Task;
-import presenter.DataBaseHelper;
-import presenter.MyDatabase;
+import java.text.SimpleDateFormat;
+
+
 
 public class Fragment1 extends Fragment {
 
-    int i = 0;
+
+    private Button play;
+    private Button pause;
+    private TextView textView;
+    private SeekBar mSeekBar;
+    private SimpleDateFormat time = new SimpleDateFormat("m:ss");
+    private MediaPlayer mediaPlayer;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.list_main, container, false);
-        Button play = (Button) view.findViewById(R.id.play);
-        Button pause = (Button) view.findViewById(R.id.pause);
-        Button stop = (Button) view.findViewById(R.id.Stop);
-        CheckBox senlin = (CheckBox) view.findViewById(R.id.senlin);
-        CheckBox xiaye = (CheckBox) view.findViewById(R.id.summer_night);
-        CheckBox cunyu = (CheckBox) view.findViewById(R.id.spring_rain);
-        CheckBox luhuo = (CheckBox) view.findViewById(R.id.stove_fire);
-        CheckBox haitan = (CheckBox) view.findViewById(R.id.beach);
 
-        //森林音效状态
-        senlin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    i = 1;
-                    /*ckOK.setEnabled(false);*/
-                }else{
-                    i = 0;
-                }
-            }
-        });
-
-        //森林音效状态
-        xiaye.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    i = 2;
-                    /*ckOK.setEnabled(false);*/
-                }else{
-                    i = 0;
-                }
-            }
-        });
-
-        //森林音效状态
-        luhuo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    i = 3;
-                    /*ckOK.setEnabled(false);*/
-                }else{
-                    i = 0;
-                }
-            }
-        });
-
-        //森林音效状态
-        haitan.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    i = 4;
-                    /*ckOK.setEnabled(false);*/
-                }else{
-                    i = 0;
-                }
-            }
-        });
-
-        //森林音效状态
-        cunyu.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked){
-                    i = 5;
-                    /*ckOK.setEnabled(false);*/
-                }else{
-                    i = 0;
-                }
-            }
-        });
-        final MediaPlayer mediaPlayer;
+        play = (Button) view.findViewById(R.id.play);
+        pause = (Button) view.findViewById(R.id.pause);
         mediaPlayer = MediaPlayer.create(getActivity(),R.raw.spring_rain);
-        // 监听音频播放完的代码，实现音频的自动循环播放
+        mSeekBar = (SeekBar) view.findViewById(R.id.seekbar);
+        textView = (TextView) view.findViewById(R.id.text);
+
+        Button senlin = (Button) view.findViewById(R.id.senlin);
+        Button xiaye = (Button) view.findViewById(R.id.summer_night);
+        Button chunyu = (Button) view.findViewById(R.id.spring_rain);
+        Button luhuo = (Button) view.findViewById(R.id.stove_fire);
+        Button haitan = (Button) view.findViewById(R.id.beach);
+        Button clock = (Button) view.findViewById(R.id.clock_second);
+
+        //森林
+        senlin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();
+                mediaPlayer=MediaPlayer.create(getActivity(),R.raw.forest);
+                //获取音乐时间
+                int max = mediaPlayer.getDuration();
+                mSeekBar.setMax(max);
+                textView.setText(formatTime(max));
+            }
+        });
+        //春雨
+        chunyu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();
+                mediaPlayer=MediaPlayer.create(getActivity(),R.raw.spring_rain);
+                //获取音乐时间
+                int max = mediaPlayer.getDuration();
+                mSeekBar.setMax(max);
+                textView.setText(formatTime(max));
+            }
+        });
+        //炉火
+        luhuo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              mediaPlayer.reset();
+              mediaPlayer=MediaPlayer.create(getActivity(),R.raw.stove_fire);
+              //获取音乐时间
+                int max = mediaPlayer.getDuration();
+                mSeekBar.setMax(max);
+                textView.setText(formatTime(max));
+            }
+        });
+        //海滩
+        haitan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();
+                mediaPlayer=MediaPlayer.create(getActivity(),R.raw.beach);
+                //获取音乐时间
+                int max = mediaPlayer.getDuration();
+                mSeekBar.setMax(max);
+                textView.setText(formatTime(max));
+            }
+        });
+        //夏夜
+        xiaye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    mediaPlayer.reset();
+                    mediaPlayer= MediaPlayer.create(getActivity(),R.raw.summer_night);
+                //获取音乐时间
+                int max = mediaPlayer.getDuration();
+                mSeekBar.setMax(max);
+                textView.setText(formatTime(max));
+            }
+        });
+
+        //时钟
+        clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.reset();
+                mediaPlayer= MediaPlayer.create(getActivity(),R.raw.clock_second);
+                //获取音乐时间
+                int max = mediaPlayer.getDuration();
+                mSeekBar.setMax(max);
+                textView.setText(formatTime(max));
+            }
+        });
 
         play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                    mediaPlayer.start();//开始播放
-                    Toast.makeText(getActivity(), "音乐播放", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                mediaPlayer.start();
+                mediaPlayer.setLooping(true);
+                //调用run开始更新进度条
+                handler.post(run);
             }
         });
         pause.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                mediaPlayer.pause();//音乐暂停
-                Toast.makeText(getActivity(), "音乐暂停", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                mediaPlayer.pause();
             }
         });
-        stop.setOnClickListener(new View.OnClickListener() {
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onClick(View v) {
-
-                   mediaPlayer.stop();
-                   Toast.makeText(getActivity(), "音乐停止", Toast.LENGTH_SHORT).show();
-
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
 
             }
-        });
-        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
             @Override
-            public void onCompletion(MediaPlayer arg0) {
-                mediaPlayer.start();
-                mediaPlayer.setLooping(true);
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                handler.removeCallbacks(run);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                mediaPlayer.seekTo(seekBar.getProgress());
+                handler.postDelayed(run, 1000);
             }
         });
 
         return view;
     }
+
+    Handler handler = new Handler();
+    Runnable run = new Runnable() {
+        @Override
+        public void run() {
+            //player.getCurrentPosition()获取音乐的当前进度
+            mSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+            handler.postDelayed(run, 100);
+        }
+    };
+
+    /**
+     * 将毫秒转化为 分钟：秒 的格式
+     *
+     * @param millisecond 毫秒
+     * @return
+     */
+    public String formatTime(long millisecond) {
+        int minute;//分钟
+        int second;//秒数
+        minute = (int) ((millisecond / 1000) / 60);
+        second = (int) ((millisecond / 1000) % 60);
+        if (minute < 10) {
+            if (second < 10) {
+                return "0" + minute + ":" + "0" + second;
+            } else {
+                return "0" + minute + ":" + second;
+            }
+        }else {
+            if (second < 10) {
+                return minute + ":" + "0" + second;
+            } else {
+                return minute + ":" + second;
+            }
+        }
+    }
+
 }
+
--- Inventory/app/src/main/java/com/example/user/inventory/Fragment2.java
+++ Inventory/app/src/main/java/com/example/user/inventory/Fragment2.java
@@ -1,41 +1,83 @@
 package com.example.user.inventory;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Fragment2 extends Fragment {
     private TextView textView;
+    private TextView hello;
     private Button button;
     private int t;
-    private int min = 25;
-    private int sec = 60;
+    //private int min = 1;
+    private int sec = 10;
+    //private SeekBar mSeekBar;
+    private TextView time_now;
+    private TimeCount time;
+    private long Count_time;
+    private String s;
+
+
+
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.clock,container,false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.clock, container, false);
         textView = (TextView) view.findViewById(R.id.clock_time);
+        hello = (TextView) view.findViewById(R.id.hello);
         button = (Button) view.findViewById(R.id.clock_bt);
-        t = 0;
+        //mSeekBar = (SeekBar) view.findViewById(R.id.seekbar);
+        time_now = (TextView) view.findViewById(R.id.time_now);
+        textView.setText("00:10");
+
+        //获取系统的日期
+        Calendar calendar = Calendar.getInstance();
+        //小时
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        if(hour>=6&&hour<=12){
+            hello.setText("早上");
+        }else if(hour>12&&hour<=6){
+            hello.setText("下午");
+        }else {
+            hello.setText("晚上");
+        }
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日");// HH:mm:ss
+        //获取当前时间
+        Date date = new Date(System.currentTimeMillis());
+        time_now.setText(simpleDateFormat.format(date));
+
 
         textView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                 builder.setTitle("设置番茄时间：");
                 // 通过LayoutInflater来加载一个xml的布局文件作为一个View对象
-                View view1 = LayoutInflater.from(getContext()).inflate(R.layout.time_dialog, null);
+                View view1 = LayoutInflater.from(getActivity()).inflate(R.layout.time_dialog, null);
                 // 设置我们自己定义的布局文件作为弹出框的Content
                 builder.setView(view1);
 
@@ -47,10 +89,11 @@ public class Fragment2 extends Fragment {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 //确定操作的内容
-                                Toast.makeText(getActivity(),"点击确定",Toast.LENGTH_SHORT).show();
-                                String s1 = m.getText().toString();
-                                min = Integer.parseInt(s1);
-                                textView.setText(min+":00");
+                                Log.v("Tag","3");
+                                s = m.getText().toString();
+                                Log.v("Tag",s);
+                                textView.setText(s + ":00");
+                                Toast.makeText(getActivity(), "点击确定", Toast.LENGTH_SHORT).show();
                             }
                         });
 
@@ -59,7 +102,7 @@ public class Fragment2 extends Fragment {
 
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getActivity(),"点击取消",Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getActivity(), "点击取消", Toast.LENGTH_SHORT).show();
                             }
                         });
                 builder.show();
@@ -67,47 +110,29 @@ public class Fragment2 extends Fragment {
             }
         });
 
-        button.setOnClickListener(new View.OnClickListener(){
+        time = new TimeCount(sec*1000,1000);
+
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
-                if(t==0){
-                    t=1;
-                    timer.start();
-                    Toast.makeText(getActivity(),"开始倒计时",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                if (t == 0) {
+                    t = 1;
+                    time.start();
+                    Toast.makeText(getActivity(), "开始倒计时", Toast.LENGTH_SHORT).show();
                     button.setText("放弃番茄");
-                }else {
-                    t=0;
-                    timer.cancel();
+                } else {
+                    t = 0;
+                    time.cancel();
                     button.setText("开始专注");
-                    textView.setText("25:00");
-                    Toast.makeText(getActivity(),"番茄钟取消",Toast.LENGTH_SHORT).show();
+                    textView.setText("00:10");
+                    Toast.makeText(getActivity(), "番茄钟取消", Toast.LENGTH_SHORT).show();
                 }
 
             }
         });
         return view;
     }
-    /**
-     * 倒数计时器
-     */
-    private CountDownTimer timer = new CountDownTimer(min * sec * 1000, 1000) {
-        /**
-         * 固定间隔被调用,就是每隔countDownInterval会回调一次方法onTick
-         * @param millisUntilFinished
-         */
-        @Override
-        public void onTick(long millisUntilFinished) {
-            textView.setText(formatTime(millisUntilFinished));
-        }
 
-        /**
-         * 倒计时完成时被调用
-         */
-        @Override
-        public void onFinish() {
-            textView.setText("00:00");
-        }
-    };
 
     /**
      * 将毫秒转化为 分钟：秒 的格式
@@ -135,18 +160,39 @@ public class Fragment2 extends Fragment {
         }
     }
 
-    /**
-     * 取消倒计时
-     */
-    public void timerCancel() {
-        timer.cancel();
-    }
 
-    /**
-     * 开始倒计时
-     */
-    public void timerStart() {
-        timer.start();
+    /* 定义一个倒计时的内部类 */
+    class TimeCount extends CountDownTimer {
+        public TimeCount(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);// 参数依次为总时长,和计时的时间间隔
+        }
+
+        @Override
+        public void onFinish() {// 计时完毕时触发
+            textView.setText("00:00");
+            AlertDialog dialog = new AlertDialog.Builder(getActivity())
+                    .setTitle("番茄时钟")//设置对话框的标题
+                    .setMessage("时间到了，休息一下吧！")//设置对话框的内容
+                    //设置对话框的按钮
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getActivity(), "点击了确定的按钮", Toast.LENGTH_SHORT).show();
+                            button.setText("开始专注");
+                            textView.setText("00:10");
+                            t=0;
+                            dialog.dismiss();
+                        }
+                    }).create();
+            dialog.show();
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {// 计时过程显示
+            textView.setText(formatTime(millisUntilFinished));
+            Count_time = millisUntilFinished;
+        }
+
     }
 
 }
